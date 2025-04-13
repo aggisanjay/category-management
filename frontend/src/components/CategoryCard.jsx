cdef4efadf956d6a9cb6a6c8cf59acf91e757242
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-04-13 12:09:08 +0530

    changes in dashboard

--- frontend/src/components/CategoryCard.jsx
+++ frontend/src/components/CategoryCard.jsx
@@ -35,7 +35,7 @@ const CategoryCard = ({ category, onUpdated }) => {
     <div className="category-card">
       <div className="category-image">
         <img
-          src={category.imageUrl ? `https://category-management-seven.vercel.app/${category.imageUrl}` : '/images/default-image.jpg'}
+          src={category.imageUrl ? `https://category-management-fvw8.onrender.com/${category.imageUrl}` : '/images/default-image.jpg'}
           alt={category.name}
         />
       </div>

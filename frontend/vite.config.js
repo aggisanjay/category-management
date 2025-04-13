7db0ca2a6070ccf66ca1705400473532d86ce764
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-04-13 14:10:36 +0530

    changes in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true,
+    port: 4173, // or any fallback
+    allowedHosts: ['category-management-1-95g0.onrender.com']
+  }
+});
